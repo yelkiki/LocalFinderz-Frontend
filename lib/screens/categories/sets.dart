fe9ef10873de49ed0b3be0ecb5acb8bba709a834
yelkiki <y.d.elkiki@gmail.com>
2023-12-26 06:59:10 +0200

    Category Products

--- lib/screens/categories/sets.dart
+++ lib/screens/categories/sets.dart
@@ -1,14 +1,114 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names, sized_box_for_whitespace, avoid_print
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:local_finderzzz/features/register/toast.dart';
 import 'package:local_finderzzz/utils/size_config.dart';
 import 'package:local_finderzzz/utils/widgets/constants.dart';
+import 'package:http/http.dart' as http;
 
-class Sets extends StatelessWidget {
-  Sets({super.key});
 
-  final List<String> items = List.generate(20, (index) => 'Item ${index + 1}');
+
+class Product {
+  final int id;
+  final String name;
+  final int price;
+  final String sex;
+  final String image;
+  final int quantity;
+  final int categoryId;
+  final int brandId;
+  final Brand brand;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.sex,
+    required this.image,
+    required this.quantity,
+    required this.categoryId,
+    required this.brandId,
+    required this.brand,
+  });
+}
+
+class Brand {
+  final String name;
+
+  Brand({required this.name});
+}
+
+List<Product> parseProducts(List<Map<String, dynamic>> productList) {
+  return productList.map((data) {
+    return Product(
+      id: data['id'] as int,
+      name: data['name'] as String,
+      price: data['price'] as int,
+      sex: data['sex'] as String,
+      image: data['image'] as String,
+      quantity: data['quantity'] as int,
+      categoryId: data['categoryId'] as int,
+      brandId: data['brandId'] as int,
+      brand: Brand(name: data['brand']['name'] as String),
+    );
+  }).toList();
+}
+
+class Sets extends StatefulWidget {
+  const Sets({super.key});
+
+  @override
+  State<Sets> createState() => _SetsState();
+}
+
+class _SetsState extends State<Sets> {
+  
+    List<Product> items = [];
+
+   @override
+  void initState() {
+    super.initState();
+    _DisplaySets(context);
+  }
+
+ Future<void> _DisplaySets(BuildContext context) async {
+  final Uri url = Uri.parse('http://10.0.2.2:3000/product/category');
+  final response = await http.post(
+    url,
+    headers: <String, String>{ 
+      'Content-Type': 'application/json; charset=UTF-8',
+      // "authorization":token!, 
+    }, body: jsonEncode(<String, dynamic>{
+      "category":"Sets"
+    },
+  ));
+
+  final Map<String, dynamic> decodedBody = json.decode(response.body);
+  final int? statusCode = decodedBody['statusCode'];
+  final String? message = decodedBody['message'];
+  
+  // Check if 'data' key exists and if it's a non-null list
+  if (decodedBody.containsKey('data') && decodedBody['data'] is List) {
+    final List<Map<String, dynamic>> fetchedItems =
+      List<Map<String, dynamic>>.from(decodedBody['data']);
+
+    if (statusCode == 200) {
+      setState(() {
+        items = parseProducts(fetchedItems);
+      });
+      // showToast(message: "$message");
+    } else {
+      showToast(message: "Error $message");
+    }
+  } else {
+    showToast(message: "Invalid data received");
+  }
+}
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -47,90 +147,65 @@ class Sets extends StatelessWidget {
                 kMainColor,
                 thirdColor,
                 kMainColor,
-              ]
+              ],
             ),
           ),
-          child: Column(
-
-            children: [
-
-              // SizedBox(
-              //   height: SizeConfig.defaultSize! * 2,
-              // ),
-
-              // Container(
-              //   height: SizeConfig.defaultSize! * 5,
-              //   width: SizeConfig.defaultSize! * 30,
-              //   child: TextFormField(
-              //       // controller: ,
-              //       textAlign: TextAlign.start,                  
-              //       decoration: InputDecoration(                   
-              //         filled: true,
-              //         fillColor: Colors.grey.shade100,
-              //         hintText: "Search all products",    
-              //         hintStyle: TextStyle(
-              //           fontSize: 13.sp,
-              //           color: kMainColor,
-              //           fontStyle: FontStyle.italic,
-              //           // fontWeight: FontWeight.bold,                     
-              //         ),                    
-              //         prefixIcon: Icon(
-              //           Icons.search,
-              //           color: kMainColor,                                      
-              //         ),         
-              //         border: InputBorder.none,                    
-              //         enabledBorder: OutlineInputBorder(
-              //           borderSide: BorderSide(
-              //             color: kMainColor,
-              //           ),
-              //           borderRadius: BorderRadius.circular(30),
-              //         ),
-              //         focusedBorder: OutlineInputBorder(
-              //           borderSide: BorderSide(
-              //             color: thirdColor,
-              //           ),
-              //           borderRadius: BorderRadius.circular(30),
-              //         ),
-                      
-              //       ),
-              //     ),
-              // ),
-
-              SizedBox(
-                height: SizeConfig.defaultSize! * 2,
+          child: Container(
+            width: SizeConfig.defaultSize! * 35,
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 15.0,
+                mainAxisSpacing: 10.0,
               ),
-
-              Expanded(
-                child: Container(
-                  width: SizeConfig.defaultSize! * 35,
-                  
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 15.0,
-                      mainAxisSpacing: 10.0,
-                    ),                          
-                    itemCount: items.length,
-                    itemBuilder: (BuildContext context, int index) {
-                
-                      return Card(
-                        child: Center(
-                          child: ListTile(
-                            title: Text(items[index]),
-                            onTap: () {
-                              // Handle item tap
-                              print('Tapped on: ${items[index]}');
-                            },
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                Product item = items[index];
+                return Card(
+                  child: InkWell(
+                    onTap: () {
+                      // Handle item tap
+                      print('Tapped on: ${item.id}');
+                      // Navigator.pushNamed(context,'/productDetails',arguments:item.id)
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                item.name,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                'Price: \$${item.price}',
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                'Brand: ${item.brand.name}',
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      );
-                    },
+                      ],
+                    ),
                   ),
-                ),
-              ),
-
-              
-            ],
+                );
+              },
+            ),
           ),
         ),
         
